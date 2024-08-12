38ca0f65f98d6d9edc79a398ae74ca2caac36352
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-08-12 09:42:46 +0100

    Update Routes.js

--- Routes.js
+++ Routes.js
@@ -1,9 +1,11 @@
 import express, { request, response } from "express";
-import {AllJoueurs,JoueurByid,SupprimerJoueur,AjouterJoueur,ModifierJoueur} from "./actions.js"
+import {AllJoueurs,JoueurByid,SupprimerJoueur,AjouterJoueur,ModifierJoueur} from "./actions.js";
+
 const router = express.Router();
 router.get("/", AllJoueurs);
 router.get("/:id", JoueurByid);
 router.delete("/:id", SupprimerJoueur);
 router.post("/", AjouterJoueur);
 router.put("/:id", ModifierJoueur);
+
 export default router;
