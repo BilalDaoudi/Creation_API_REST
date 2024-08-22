78822a2bef71fdc52b75da41b7b2eeb974b4c0b6
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-08-22 09:14:56 +0100

    Update actions.js

--- actions.js
+++ actions.js
@@ -1,11 +1,7 @@
 import joueurs from './joueurs.json' assert {type:'json'};
-
-
 export const AllJoueurs = (request, response) => {
   response.json(joueurs);
 };
-
-
 export const JoueurByid = (request, response) => {
   const id = request.params.id;
   const joueur = joueurs.find((p) => p.id === id);
@@ -15,8 +11,6 @@ export const JoueurByid = (request, response) => {
     response.send("n'est pas joueur avec id donnée");
   }
 };
-
-
 export const SupprimerJoueur = (request, response) => {
   const id = request.params.id;
   let joueur = joueurs.find((j) => j.id === id);
@@ -28,15 +22,11 @@ export const SupprimerJoueur = (request, response) => {
   }
   response.json(joueurs);
 };
-
-
 export const AjouterJoueur = (request, response) => {
   const joueur = request.body;
   joueurs.push(joueur);
   response.send(`joueur avec CIN : ${joueur.id} a été Ajoutée `);
 };
-
-
 export const ModifierJoueur = (request, response) => {
   const id = request.params.id;
   const { idEquipe, Nom, Numero } = request.body;
