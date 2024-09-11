38b4300bc415764ba4b3ac14597d46673c94157a
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-09-12 03:09:25 +0800

    Update Routes.js

--- Routes.js
+++ Routes.js
@@ -1,5 +1,7 @@
 import express, { request, response } from "express";
+
 import {AllJoueurs,JoueurByid,SupprimerJoueur,AjouterJoueur,ModifierJoueur} from "./actions.js";
+
 const router = express.Router();
 
 router.get("/", AllJoueurs);
