8b76aa5d9f8e392bb5e4e3a7888d14f6c5b1cb75
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-09-12 03:10:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 import express, { request, response } from "express";
-
 import routes from "./Routes.js";
-
 const app = express() ;
 
 app.use(express.json());
-
 app.get("/", (request, response)=>{
     response.send('Créer API REST');
 });
-
 app.use("/joueurs",routes);
-
 app.listen(5000, ()=> console.log(`The server is running on port 5000`));
